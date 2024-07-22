7ed0c41b795a986989917560e21b86591019bbc2
Marco Grochowiak <marco.grochowiak@outlook.com>
2024-07-22 11:27:20 +0200

    2. Iteration: Polymorphie

--- com/cc/java/Circle.java
+++ com/cc/java/Circle.java
@@ -2,6 +2,16 @@ package com.cc.java;
 
 public class Circle extends Shape {
 
+    private double radius;
 
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+	@Override
+	public double area() {
+        // return Math.PI * radius * radius;  
+		return Math.PI * Math.pow(radius, 2);
+	} 
 
 }
--- com/cc/java/Triangle.java
+++ com/cc/java/Triangle.java
@@ -2,6 +2,15 @@ package com.cc.java;
 
 public class Triangle extends Shape {
 
+    private double base, height;
 
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+    @Override
+    public double area() {
+        return 0.5 * base * height;
+    }
     
 }
